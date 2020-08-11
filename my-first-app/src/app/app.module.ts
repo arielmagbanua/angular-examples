a1dfd8314602a6fa100ed887aa0d885c86ec4c0c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-11 23:31:51 +0800

    Added my first app.

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, ServerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, ServerComponent]
+})
+export class AppModule { }
