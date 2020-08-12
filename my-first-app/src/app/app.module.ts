8a03cfce2ac060787aa672d5364cc35012560d73
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-12 20:49:57 +0800

    Added assignment components.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent, ServersComponent
+    AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent
   ],
   imports: [
     BrowserModule
