724493ebb6b7e14100dd91bacc8255369f9c1a5c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-14 22:34:05 +0800

    Laid out the content of the components.

--- food-shop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-shop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg'
+    ),
+  ];
 
   constructor() { }
 
