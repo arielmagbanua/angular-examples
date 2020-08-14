4e2b158d29071943f566137a402b4a64fe19e1ca
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-14 23:29:22 +0800

    Supress the linting.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  // tslint:disable-next-line:no-input-rename
   @Input('srvElement')
   element: {
     type: string,
