98fb421e4ffa75a0023c4e7ed0790e6cd6f33d31
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-15 21:52:10 +0800

    Switching of pages by event emitter.

--- food-shop/src/app/app.component.ts
+++ food-shop/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'food-shop';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
 }
--- food-shop/src/app/header/header.component.ts
+++ food-shop/src/app/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output()
+  featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
+  }
 }
