b222b434faf9f5a9cb1c8238c75d0a08824cd322
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-16 22:23:04 +0800

    Loading of recipes.

--- food-shop/src/app/recipes/recipe.service.ts
+++ food-shop/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,24 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg'
+      'Lumpia',
+      'Filipino Lumpia',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6244851.jpg',
+      [
+        new Ingredient('Lumpia Wrapper', 2),
+        new Ingredient('Bamboo Shoot', 2),
+        new Ingredient('Pork', 3)
+      ]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg'
+      'Burger',
+      'Minute Burger',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Tomatoes', 2),
+        new Ingredient('Beef', 3)
+      ]
     ),
   ];
 
