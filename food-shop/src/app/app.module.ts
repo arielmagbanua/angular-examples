2cbabe54b04f2a1496977ccad50ecd10c70a2e0f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-16 16:47:08 +0800

    Added dropdown directive to open and close the dropdown.

--- food-shop/src/app/app.module.ts
+++ food-shop/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingEditComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
