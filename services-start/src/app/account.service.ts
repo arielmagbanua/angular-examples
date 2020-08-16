d6bb8af0ae12a27d2abd29e549745c88aa1f1489
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-16 18:18:51 +0800

    Injecting service to a service.

--- services-start/src/app/account.service.ts
+++ services-start/src/app/account.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class AccountService {
   accounts = [
     {
@@ -14,11 +18,15 @@ export class AccountService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) { }
+
   addAccount(name: string, status: string): void {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string): void {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
