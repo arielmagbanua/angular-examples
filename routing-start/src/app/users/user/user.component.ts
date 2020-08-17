682d00d9465530061a6ef9e1dcca8abb9fe5bcb0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-17 21:01:36 +0800

    Unsubscribing observables.

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,9 +19,14 @@ export class UserComponent implements OnInit {
      name: this.route.snapshot.params['name']
    };
 
-   this.route.params.subscribe((params: Params) => {
+   this.paramsSubscription = this.route.params
+     .subscribe((params: Params) => {
      this.user.id = params['id'];
      this.user.name = params['name'];
    });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
