d2ca06741ca7b0bc6e1cb7b1dde7b5aaa2357789
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-18 21:37:13 +0800

    Populated the app routing module.

--- food-shop/src/app/app-routing.module.ts
+++ food-shop/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes'
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
