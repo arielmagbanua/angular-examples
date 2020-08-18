1fd6d8ffad79f7b1bd4ccf556b14fd0a2eade923
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-18 20:02:54 +0800

    Added auth guard.

---
+++ routing-start/src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
