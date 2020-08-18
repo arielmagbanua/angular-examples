c8c3642e1bd4309cf118893b32358eb579a076de
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-18 21:05:10 +0800

    Added resolver.

---
+++ routing-start/src/app/servers/server/server-resolver.service.ts
@@ -0,0 +1,26 @@
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { ServersService } from '../servers.service';
+import { Injectable } from '@angular/core';
+
+interface Server {
+  id: number;
+  name: string;
+  status: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class ServerResolver implements Resolve<Server> {
+  constructor(private serversService: ServersService) { }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Server> | Promise<Server> | Server {
+    return this.serversService.getServer(+route.params['id']);
+  }
+}
