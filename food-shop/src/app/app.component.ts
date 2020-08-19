ca2cbf45897996cd2a687107ef00a3e52c9ccdff
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-19 20:34:05 +0800

    Code clean up.

--- food-shop/src/app/app.component.ts
+++ food-shop/src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'food-shop';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string): void {
-    this.loadedFeature = feature;
-  }
 }
