c87bfa85ea9096ed5f138ee8ab0a8a865a8c274f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-20 20:40:41 +0800

    Set value with form instance object.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,6 +15,15 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });
   }
 
   // onSubmit(form: NgForm) {
