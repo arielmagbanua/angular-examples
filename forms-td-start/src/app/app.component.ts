fe2a9ba57ee5be7f929f455cfee6f238a116d668
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-20 20:31:28 +0800

    ng model group.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   // @ts-ignore
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
