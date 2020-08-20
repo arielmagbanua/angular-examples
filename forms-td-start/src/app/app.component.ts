e440577eb1b9094054196a209b356ebb5d21d13d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-20 20:50:07 +0800

    Displayed form data.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,6 +43,11 @@ export class AppComponent {
   //   console.log(form);
   // }
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
