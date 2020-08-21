8e85171cac35c9325f8c0180479a897afe193cb3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-22 01:59:10 +0800

    Subscribing to form changes, set value, and patch value.

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -23,6 +23,29 @@ export class AppComponent implements OnInit {
         new FormControl(null)
       ])
     });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    this.signupForm.statusChanges.subscribe((value) => {
+      console.log(value);
+    });
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Ariel',
+        'email': 'test@ambot.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Ariel',
+      }
+    });
   }
 
   onSubmit() {
