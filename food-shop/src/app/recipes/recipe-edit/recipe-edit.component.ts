4dd79ba8738f16dd00781beec9cd8742e0cbd406
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-22 21:03:26 +0800

    Loaded the ingredients using reactive approach.

--- food-shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ food-shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import {FormControl, FormGroup} from '@angular/forms';
+import {AbstractControl, FormArray, FormControl, FormGroup} from '@angular/forms';
 import {RecipeService} from '../recipe.service';
 
 @Component({
@@ -30,22 +30,47 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onAddIngredient(): void {
+    (this.recipeForm.get('ingredients') as FormArray).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl(1)
+      })
+    );
+  }
+
   private initForm(): void {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    const recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe[`ingredients`]) {
+        for (const ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription)
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients
     });
+  }
+
+  get ingredientControls(): AbstractControl[] {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
 }
