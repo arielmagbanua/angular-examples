6472cf05f9f64a847c17962717743ef9b8823364
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-22 23:02:37 +0800

    Filter pipe.

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -32,11 +32,22 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+  filteredStatus = '';
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2)
+    });
   }
 }
