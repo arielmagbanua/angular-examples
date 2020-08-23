a245994a19d8d351618f36d576d0c8ab1c976da2
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 16:08:28 +0800

    Headers and params.

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -29,7 +29,18 @@ export class PostsService {
   }
 
   fetchPosts() {
-    return this.http.get<{ [key: string]: Post }>('https://udemy-training-af9d1.firebaseio.com/posts.json')
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('foo', 'bar');
+    return this.http.get<{ [key: string]: Post }>(
+      'https://udemy-training-af9d1.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({
+          'Custom-Header': 'Hello'
+        }),
+        params: searchParams
+      }
+    )
       .pipe(map((responseData) => {
         const postsArray: Post[] = [];
         for (const key in responseData) {
