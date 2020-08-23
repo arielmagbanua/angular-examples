3cce47dae6c489353f32d0fb843aeeb07ac131d5
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 19:45:55 +0800

    Initialized non existing ingredients array.

--- food-shop/src/app/recipes/recipe.service.ts
+++ food-shop/src/app/recipes/recipe.service.ts
@@ -10,28 +10,30 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Lumpia',
-      'Filipino Lumpia',
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6244851.jpg',
-      [
-        new Ingredient('Lumpia Wrapper', 2),
-        new Ingredient('Bamboo Shoot', 2),
-        new Ingredient('Pork', 3)
-      ]
-    ),
-    new Recipe(
-      'Burger',
-      'Minute Burger',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Tomatoes', 2),
-        new Ingredient('Beef', 3)
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Lumpia',
+  //     'Filipino Lumpia',
+  //     'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6244851.jpg',
+  //     [
+  //       new Ingredient('Lumpia Wrapper', 2),
+  //       new Ingredient('Bamboo Shoot', 2),
+  //       new Ingredient('Pork', 3)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Burger',
+  //     'Minute Burger',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Tomatoes', 2),
+  //       new Ingredient('Beef', 3)
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
 
--- food-shop/src/app/shared/data-storage.service.ts
+++ food-shop/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +25,11 @@ export class DataStorageService {
 
   fetchRecipes(): void {
     this.http.get<Recipe[]>('https://udemy-training-af9d1.firebaseio.com/recipes.json')
+      .pipe(map((recipes) => {
+        return recipes.map((recipe) => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe((recipes) => {
         console.log(recipes);
         this.recipeService.setRecipes(recipes);
