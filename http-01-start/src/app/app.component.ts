93318bc637424159c5aa9232b8b1fd0735eac4d9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 15:06:03 +0800

    Moved the sending and fetching of posts to posts service.

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import {Post} from './post.model';
+import { Post } from './post.model';
+import {PostsService} from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,49 +12,30 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe((posts: Post[]) => {
+      this.loadedPosts = posts;
+      this.isFetching = false;
+    });
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http
-      .post<{ name: string }>(
-        'https://udemy-training-af9d1.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe((responseData) => {
-        console.log(responseData);
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
+    this.isFetching = true;
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPosts().subscribe((posts: Post[]) => {
+      this.loadedPosts = posts;
+      this.isFetching = false;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http.get<{ [key: string]: Post }>('https://udemy-training-af9d1.firebaseio.com/posts.json')
-      .pipe(map((responseData) => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({...responseData[key], id: key});
-          }
-        }
-
-        return postsArray;
-      }))
-      .subscribe((posts: Post[]) => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
   }
 }
---
+++ http-01-start/src/app/posts.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) { }
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title, content };
+
+    // Send Http request
+    this.http
+      .post<{ name: string }>(
+        'https://udemy-training-af9d1.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post }>('https://udemy-training-af9d1.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+
+        return postsArray;
+      }));
+  }
+}
