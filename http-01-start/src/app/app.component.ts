abbc892067429b95e78f10c69bce5682d54a5fb0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 14:50:44 +0800

    Added post model and fetching of posts.

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -16,10 +18,10 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://udemy-training-af9d1.firebaseio.com/posts.json',
         postData
       )
@@ -38,9 +40,10 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://udemy-training-af9d1.firebaseio.com/posts.json')
+    this.isFetching = true;
+    this.http.get<{ [key: string]: Post }>('https://udemy-training-af9d1.firebaseio.com/posts.json')
       .pipe(map((responseData) => {
-        const postsArray = [];
+        const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postsArray.push({...responseData[key], id: key});
@@ -49,8 +52,9 @@ export class AppComponent implements OnInit {
 
         return postsArray;
       }))
-      .subscribe((posts) => {
-        console.log(posts);
+      .subscribe((posts: Post[]) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
       });
   }
 }
