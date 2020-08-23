62843cbf73809066f877391205383aeb52f00575
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 15:24:15 +0800

    Handling error with subject.

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import {PostsService} from './posts.service';
+import {generateErrorMessage} from 'codelyzer/angular/styles/cssLexer';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
-  constructor(private http: HttpClient, private postsService: PostsService) {}
+  constructor(
+    private http: HttpClient,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe((errorMessage: string) => {
+      this.error = errorMessage;
+    });
+
     this.isFetching = true;
     this.postsService.fetchPosts().subscribe((posts: Post[]) => {
       this.loadedPosts = posts;
@@ -46,5 +56,9 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -21,6 +23,8 @@ export class PostsService {
       )
       .subscribe((responseData) => {
         console.log(responseData);
+      }, (error) => {
+        this.error.next(error.message);
       });
   }
 
