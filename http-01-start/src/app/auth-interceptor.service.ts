7b62c8c312e175739a6d0c81cae4d675a0058279
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 16:46:21 +0800

    Multiple iterceptors.

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -8,7 +8,9 @@ import { Observable } from 'rxjs';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Request is on its way');
-    return next.handle(req);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
   }
 }
