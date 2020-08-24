7a3808029ebe6b6393a7e1e0f4d513ec418f7023
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-24 20:44:40 +0800

    Refined the template and added route and links.

--- food-shop/src/app/app-routing.module.ts
+++ food-shop/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import {AuthComponent} from './auth/auth.component';
 
 const routes: Routes = [
   {
@@ -44,6 +45,10 @@ const routes: Routes = [
   {
     path: 'shopping-list',
     component: ShoppingListComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
   }
 ];
 
