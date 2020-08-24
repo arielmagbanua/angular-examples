d1655a2f398d2a4fcdb2242d270ab648e32c317d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-24 21:24:11 +0800

    Finished the sign up user.

--- food-shop/src/app/auth/auth.component.ts
+++ food-shop/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +10,7 @@ import {NgForm} from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLoginMode = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +20,25 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm): void {
-    console.log(form.value);
+    if (!form.value) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //
+    } else {
+      this.authService.signUp(email, password)
+        .subscribe((responseData) => {
+            console.log(responseData);
+          },
+          (error) => {
+            console.log(error);
+          });
+    }
+
     form.reset();
   }
 }
