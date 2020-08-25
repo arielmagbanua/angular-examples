ec11001283bd877ea077c5a39e877a2ad539dde0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-25 21:21:48 +0800

    Updated UI upon authentication.

--- food-shop/src/app/header/header.component.ts
+++ food-shop/src/app/header/header.component.ts
@@ -1,18 +1,31 @@
 import {
-  Component,
+  Component, OnDestroy,
   OnInit,
 } from '@angular/core';
-import {DataStorageService} from '../shared/data-storage.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+import {User} from '../auth/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-  ngOnInit(): void { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user: User) => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData(): void {
     this.dataStorageService.storeRecipes();
@@ -21,5 +34,9 @@ export class HeaderComponent implements OnInit {
   onFetchData(): void {
     this.dataStorageService.fetchRecipes()
       .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
