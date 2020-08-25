1276b034c2f24e3ac386e7bd3b66490dfd0596a5
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-25 21:12:40 +0800

    Navigation after login.

--- food-shop/src/app/auth/auth.component.ts
+++ food-shop/src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {AuthResponseData, AuthService} from './auth.service';
-import {Observable} from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -40,10 +41,11 @@ export class AuthComponent implements OnInit {
       authObs = this.authService.signUp(email, password);
     }
 
-    authObs.subscribe((responseData) => {
+    authObs.subscribe(async (responseData) => {
         console.log(responseData);
         this.isLoading = false;
         this.error = null;
+        await this.router.navigate(['/recipes']);
       },
       (errorMessage) => {
         console.log(errorMessage);
--- food-shop/src/app/auth/auth.service.ts
+++ food-shop/src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
-import {catchError, tap} from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { User } from './user.model';
 
