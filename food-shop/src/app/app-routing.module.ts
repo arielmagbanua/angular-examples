f18ce3f15af20201c8f5c2dfbb696991ce54c7da
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-27 21:56:12 +0800

    Extracted recipes routes to recipes routing module.

--- food-shop/src/app/app-routing.module.ts
+++ food-shop/src/app/app-routing.module.ts
@@ -1,50 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/recipes',
     pathMatch: 'full',
-  },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [
-      AuthGuard
-    ],
-    children: [
-      {
-        path: '',
-        component: RecipeStartComponent
-      },
-      {
-        path: 'new',
-        component: RecipeEditComponent,
-        resolve: [
-          RecipesResolverService
-        ]
-      },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [
-          RecipesResolverService
-        ]
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent
-      }
-    ]
   },
   {
     path: 'shopping-list',
