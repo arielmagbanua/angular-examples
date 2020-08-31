33ed8622ee866cd156e57edc03b70b724765f218
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-31 21:44:55 +0800

    Used add ingredients using ngrx store.

--- food-shop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ food-shop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,7 +2,9 @@ import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -16,7 +18,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>
+  ) { }
 
   ngOnInit(): void {
     this.subscription = this.slService.startedEditing.subscribe((index: number) => {
@@ -40,7 +45,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     } else {
-      this.slService.addIngredient(newIngredient);
+      // this.slService.addIngredient(newIngredient);
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
 
     this.slForm.reset();
