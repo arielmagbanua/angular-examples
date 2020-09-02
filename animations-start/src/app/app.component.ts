641cbda457753f961d569462d3dfeb9eab7c86d3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-02 22:36:05 +0800

    Multiphase animation.

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -32,7 +32,15 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', animate(300))
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange',
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
     ])
   ]
 })
